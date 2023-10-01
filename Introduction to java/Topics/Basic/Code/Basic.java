2a47a210f899db5d5cf7109e2615c2d213563c59
Manish <kumarmanishparjapati05@gmail.com>
2023-10-01 13:29:33 +0530

    update repo

--- Introduction to java/Topics/Basic/Code/Basic.java
+++ Introduction to java/Topics/Basic/Code/Basic.java
@@ -1,14 +1,57 @@
 
 // Ouestion 1. Take 2 integer values in two variables a and b and print their product.
-class Basic {
+
+/*class Basic {
+    *public static void main(String[] args) {
+
+         * int a = 20;
+         * int b = 30;
+         * int product = (a * b);
+         * System.out.println("Product of a and b is = " + product);
+         * 
+         * }
+         * }
+         */
+// output
+// Product of a and b is = 600
+
+// Question 2. Print the ASCII value of character ‘U’.
+
+/*
+ * class Basic {
+ * public static void main(String[] args) {
+ * char x = 'U';
+ * int y = x;
+ * System.out.println("The ASCII value of U is = " + y);
+ * }
+ * }
+ */
+
+// Output
+// The ASCII value of U is = 85
+
+// Question 3.- Write a Java program to take the length and breadth of a rectangle and print its area.
+
+/*import java.util.Scanner;
+
+public class Basic {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 30;
-        int product = (a * b);
-        System.out.println("Product of a and b is = " + product);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Length of rectangle");
+        int l = sc.nextInt();
+        System.out.println("Enter the breadth of the rectangle");
+        int b = sc.nextInt();
+        int area = (l * b);
+        System.out.println("The Area of the rectangle is = " + area);
 
     }
 }
-// output
 
-// Product of a and b is = 600
+Output
+Enter the Length of rectangle
+10
+Enter the breadth of the rectangle
+10
+The Area of the rectangle is = 100 */
+
+
