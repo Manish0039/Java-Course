fc323c5d1e81887a48a200b9cf0456f8036c2761
Manish <kumarmanishparjapati05@gmail.com>
2023-10-04 15:04:06 +0530

    update repo

--- Introduction to java/Topics/Data Type/Data_Type1.java
+++ Introduction to java/Topics/Data Type/Data_Type1.java
@@ -3,21 +3,23 @@
 //  Question 1. Write a Java program to convert temperature from Fahrenheit to Celsius degrees.
 
 
+/*
 import java.util.Scanner;
 public class Data_Type1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Fahrenheit value = ");
+        System.out.print(" Fahrenheit = ");
         float  Fahrenheit = sc.nextFloat();
 
-        float Celsius = (5*Fahrenheit /9) - 32f;
+        float Celsius = (Fahrenheit-32) *5/9;
         System.out.println("Celsius = "+Celsius+"Degree");
 
-
-
-
     }
-
 }
+ Output
+Fahrenheit = 100
+Celsius = 37.77778Degree
+*/
+
 
 
---
+++ out/production/Java-Course/README.md
@@ -0,0 +1,4 @@
+# Java-Course
+This is my Java repository
+<br>
+
