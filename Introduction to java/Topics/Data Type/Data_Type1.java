09956cde0e5e38cd9c343fdfca2ce7d7c6c062dd
Manish <kumarmanishparjapati05@gmail.com>
2023-10-04 15:45:22 +0530

    update repo

--- Introduction to java/Topics/Data Type/Data_Type1.java
+++ Introduction to java/Topics/Data Type/Data_Type1.java
@@ -16,10 +16,73 @@ public class Data_Type1 {
 
     }
 }
+
  Output
 Fahrenheit = 100
-Celsius = 37.77778Degree
-*/
+Celsius = 37.77778Degree */
 
+
+
+
+// Question 2.  Write a Java program that reads a number in inches and converts it to meters.
+//Note: One inch is 0.0254 meter.
+
+
+/*
+  import java.util.Scanner;
+ public class Data_Type1{
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Inches = ");
+         double inch = sc.nextDouble();
+         double meters = inch*0.0254;
+         System.out.println("Meters = "+meters);
+
+
+     }
+ }
+ // Output
+//Inches = 567
+//Meters = 14.4018  */
+
+
+
+
+// Question 3.  Write a Java program that reads an integer between 0 and 1000 and adds all the digits in the integer.
+// Without using loops,function and recursion.
+
+//<------------There are many approach to solve this question we will discuss it later------->
+
+
+/*
+import java.util.Scanner;
+
+public class Data_Type1 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the value between 0 and 1000");
+        int num = sc.nextInt();
+        int digit1 = num%10;
+        int n = num/10;
+        int digit2 = n%10;
+        n = n/10;
+        int digit3 = n%10;
+        n = n/10;
+        int digit4 = n%10;
+        n=n/10;
+        int Sum = digit1+digit2+digit3+digit4;
+        System.out.println("The Sum of the digits of the number "+ num + " is " + Sum);
+
+    }
+} */
+//Output
+//Enter the value between 0 and 1000
+//543
+//The Sum of the digits of the number 543 is 12
+
+
+
+
+// Question 4. 
 
 
