4372e74d5751b3940a547229d5bffa49709c06c4
Manish <kumarmanishparjapati05@gmail.com>
2024-01-19 10:46:44 +0530

    update repo

--- Introduction to java/Durga sir/Array_Practise.java
+++ Introduction to java/Durga sir/Array_Practise.java
@@ -23,12 +23,27 @@ public class Array_Practise {
         System.out.println(a.length);
 
 
-       for(int i = 0;i<a.length;i++){
-           for(int j = 0;j<a[i].length;j++){
+       for(int i = 0;i<a.length;i++) {
+           for (int j = 0; j < a[i].length; j++) {
 
-               System.out.println(a[i][j]);d
+               System.out.println("The value at a [" + i + "][" + j + "]" + " = " + a[i][j]);
+
            }
        }
+               //OUTPUT
+              /* 3
+               The value at a [0][0] = 1
+               The value at a [0][1] = -2
+               The value at a [0][2] = 3
+               The value at a [1][0] = -4
+               The value at a [1][1] = -5
+               The value at a [1][2] = 6
+               The value at a [1][3] = 9
+               The value at a [2][0] = 7
+
+               */
+
+
 
 
 
