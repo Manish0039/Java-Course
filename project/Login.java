179703930caf62f55691ebc35a04cba891aee7c9
Manish <kumarmanishparjapati05@gmail.com>
2024-06-12 21:08:22 +0530

    update

--- project/Login.java
+++ project/Login.java
@@ -5,8 +5,8 @@ public class Login extends JFrame{  // JFrame is a class of Swing. Here we are u
 
     Login(){  
 
-        setTitle("Automated Teller Machine");
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("images/logo.jpeg"));  // giving the location of the image, but we cannot use the image directly on frame so we are using JLabel.
+        setTitle("Automated Teller Machine");  // setTitle is used for writing the title 
+        ImageIcon i1 = new ImageIcon(ClassLoader. getSystemResource("images/logo.jpeg"));  // giving the location of the image, but we cannot use the image directly on frame so we are using JLabel.
 
         JLabel label = new JLabel(i1); // 
         add(label);
@@ -16,7 +16,7 @@ public class Login extends JFrame{  // JFrame is a class of Swing. Here we are u
 
         setSize(800,600);  // Dimension
         setVisible(true);   //  by default visibility of ṇ frame is hidden from the user so to see the login page we make its visibility true.
-        setLocation(350,150); // 
+        setLocation(300,150); // 
     }
     public static void main(String[] args) {
         new Login();  // anonymous object 
