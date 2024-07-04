3757d847aff355709fe0eef805217acf3b059730
Manish <kumarmanishparjapati05@gmail.com>
2024-07-04 21:46:27 +0530

    update

--- Introduction to java/Topics/Basic/Code/Main.java
+++ Introduction to java/Topics/Basic/Code/Main.java
@@ -383,3 +383,30 @@
  * Work is Done.
  * 
  */
+
+/*
+ * Question 13. Write a program to add the sum of first n natural number.
+ * 
+ * import java.util.Scanner;
+ * public class Main{
+ * public static void main(String[] args){
+ * Scanner sc = new Scanner(System.in);
+ * System.out.println("Enter the Number");
+ * int num = sc.nextInt();
+ * int i =1;
+ * int sum=0;
+ * while(i<=num){
+ * sum = sum+i;
+ * i++;
+ * 
+ * }
+ * System.out.println("The sum of first "+num+" Natural Number = "+sum);
+ * }
+ * }
+ * 
+ * Output
+ * 
+ * Enter the Number
+ * 5
+ * The sum of first 5 Natural Number = 15
+ */
